02500c40c59288d14b5949c754bb8fe38255f5a6
NazMzadeh <100687473+Nz020708@users.noreply.github.com>
2022-05-06 08:07:35 +0400

    Add files via upload

---
+++ String.StringBuilder/String.StringBuilder/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace String.StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Console.WriteLine(Indexof("el"));
+            //    string a = "Hello";
+            //    Console.WriteLine(a.IndexOf("el"));
+            Console.WriteLine(Reverse("How are you ?"));
+       }
+        #region FirstTask
+        static int Indexof(string word)
+        {
+            string temp = "Hello";
+            int i = 0;
+            bool check = false;
+            for (; i < temp.Length; i++)
+            {
+                if (temp[i] == word[0])
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if (!check)
+            {
+                return -1;
+            }
+            return i;
+
+        }
+
+        static int Indexof(string word, int index)
+        {
+            string temp = "Hello";
+            bool check = false;
+            int i = index;
+            for (; i < temp.Length; i++)
+            {
+                if (temp[i] == word[0])
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if (!check)
+            {
+                return -1;
+            }
+            return i;
+        }
+
+        static int Indexof(string word, int index, int count)
+        {
+            string temp = "Hello";
+            bool check = false;
+            int i = index;
+            for (; i <= index + count; i++)
+            {
+                if (temp[i] == word[0])
+                {
+                    check = true;
+                    break;
+                }
+            }
+            if (!check)
+            {
+                return -1;
+            }
+            return i;
+
+        }
+        #endregion
+        #region SecondTask
+        static string Reverse(string str)
+        {
+
+            string reverse = string.Empty;
+            foreach (var word in str.Split(' '))
+            {
+                string strword = "";
+                foreach (var charr in word)
+                {
+                    strword = charr + strword;
+                }
+                reverse = reverse + strword + " ";
+            }
+            return reverse;
+        }
+        #endregion
+
+    }
+}
